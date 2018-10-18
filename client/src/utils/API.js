674b33137c9b21efec97b4642102e1825103792b
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-10-18 19:15:10 -0400

    search and find similar

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,12 +1,21 @@
 import axios from "axios";
 const movieKey = require("key.js");
 const BASEURL = "https://api.themoviedb.org/3/search/tv";
- 
+const BASEURL2 = "https://api.themoviedb.org/3/tv/";
 
 export default {
   search: function(search) {
     return axios.get(BASEURL + "?api_key=" + movieKey  + "&query=" + search);
+  },
+
+
+  similar: function(id){
+    return axios.get(BASEURL2 + id + "/similar?api_key=" + movieKey);
   }
+
+
+
+
 };
 
 
