64da4ba9f075cb80a91ce17578e79344acdfb82e
Megan Pellegrino-Zubricky <pellegrino.megan@gmail.com>
2018-10-25 00:28:43 -0400

    added database host config

--- models/index.js
+++ models/index.js
@@ -10,6 +10,7 @@ let sequelize = new Sequelize(
   process.env.DATABASE_USERNAME,
   process.env.DATABASE_PASSWORD,
   {
+    host: process.env.DATABASE_HOST,
     dialect: process.env.DATABASE_DIALECT,
     port: process.env.DATABASE_PORT,
   }
